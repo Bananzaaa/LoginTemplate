ec17c911ab88338b52099502b72d2f6f1a531066
Stas Anatskiy <devsanatskiy@gmail.com>
2020-12-15 11:12:02 +0300

    Initial Commit

---
+++ LoginTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginTemplate
+//
+//  Created by Ацкий Станислав on 15.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
